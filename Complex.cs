6b1999b1c9fc4dc6c3af25e6af33ba00dfd072b5
FeliIsra <fisrael@est.frba.utn.edu.ar>
2019-04-22 12:49:22 -0300

    MatematicaSuperior_1.0

---
+++ Complex.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MatematicaSuperior2._0
+{
+    class Complex{
+
+        double a = 0;
+        double b = 0;
+        double modulo = 0;
+        double argumento = 0;
+    
+        
+        public void setBinomial(double _a, double _b)
+        {
+            this.a = _a;
+            this.b = _b;
+        }
+
+        public void setPolar(double _module, double _grade)
+        {
+            this.modulo= _module;
+            this.argumento = _grade;
+        }
+
+        public double getModulo()
+        {
+            return this.modulo;
+        }
+
+        public double getArgumento()
+        {
+            return this.argumento;
+        }
+
+        public double getA()
+        {
+            return this.a;
+        }
+
+        public double getB()
+        {
+            return this.b;
+        }
+
+        public void setModulo(){
+            this.modulo = Math.Sqrt(Math.Pow(this.a, 2) + Math.Pow(this.b, 2));
+        }
+
+        public void setArgumento()
+        {
+          
+          if(this.a >=0 && this.b >= 0){
+                this.argumento = Math.Atan2(this.b, this.a);
+          } else
+            {
+                if(this.a >= 0 && this.b < 0)
+                {
+                    this.argumento = Math.Atan2(-this.b, -this.a) + Math.PI;
+                } else
+                {
+                    if(this.a < 0 && this.b >= 0)
+                    {
+                        this.argumento = Math.Atan2(this.b, -this.a) + 2 * Math.PI;
+                    } else if(this.a < 0 && this.b < 0)
+                    {
+                        this.argumento = Math.Atan2(-this.b, -this.a) + Math.PI;
+                    }
+                }
+            }  
+            
+        }
+
+        public Complex binomialToPolar()
+        {
+            this.setModulo();
+            this.setArgumento();
+
+            return this;
+        }
+
+        public Complex polarToBinomial()
+        {
+            this.a = this.modulo * Math.Cos(this.argumento);
+            this.b = this.modulo * Math.Sin(this.argumento);
+
+            return this;
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_MatematicaSuperior2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //button1.Visible = false;
+           // button2.Visible = false;
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("COMPLETE AMBOS CAMPOS");
+            }
+            else
+            {
+                Complex miNum = new Complex();
+                miNum.setBinomial(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+                miNum.binomialToPolar();
+                double arg = miNum.getArgumento() / Math.PI;
+                lbl_resultado.Text = "[    " + miNum.getModulo().ToString("N2") + " , " + (arg).ToString("N2") + " π" + "]";
+            }
+           // textBox1.Text = "";
+           // textBox2.Text = "";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("COMPLETE AMBOS CAMPOS");
+            }
+            else
+            {
+                Complex miNum = new Complex();
+                miNum.setPolar(Convert.ToDouble(textBox3.Text), Convert.ToDouble(textBox4.Text));
+                miNum.polarToBinomial();
+                lbl_resultado.Text = Math.Round(miNum.getA()).ToString("N3") + " + " + Math.Round(miNum.getB()).ToString("N3") + " j";
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
